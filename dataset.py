9e03f5d93078b0fd720b6324ea781ac155a82645
erha <fikri41153.pesonaxii@gmail.com>
2023-06-07 12:45:32 +0700

    update codingan

--- dataset.py
+++ dataset.py
@@ -8,16 +8,15 @@ def modell(x):
     data_test = pd.read_csv('data/data.csv')
     data_test = data_test.drop(data_test.columns[0],axis=1)
     # memasukkan data kedalam data test
-    data_test = data_test.append(df,ignore_index = True)
-
-    print(data_test)
+    data_test = data_test.append(other = df,ignore_index = True)
     # # return data_test yang sudah dinormalisasi
+    # convert data_test to float
+    data_test = data_test.astype(float)    
+    # print(data_test)
     return joblib.load('data/nana.sav').fit_transform(data_test)
 
 
-def cluster(x):
 
-    return joblib.load('data/kmeans.pkl').fit(x).predict(x)
 def svr(x):
-    
+
     return joblib.load('data/SVRmodel.pkl').predict(x)
--- monit.py
+++ monit.py
@@ -22,9 +22,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import *
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVR
+import pymongo
 
+uri = "mongodb+srv://lalaerha:bp8GlR3fWrH7lL59@cluster0.yyqsrnb.mongodb.net/?retryWrites=true&w=majority"
 data1 = pd.read_csv('https://raw.githubusercontent.com/LALA09-erha/Python-TrainingProject/master/psd/Water%20Quality%20Testing.csv')
 data2 = pd.read_csv('https://raw.githubusercontent.com/LALA09-erha/Python-TrainingProject/master/psd/waterquality.csv', encoding='windows-1252')
+
+
+clnt = pymongo.MongoClient(uri)
+mydb = clnt['data_water']
 
 # menyeleksi hanya 500 data
 data2 = data2.head(500)
@@ -38,101 +44,119 @@ with st.sidebar:
 # with tab1:
 col1, col2 = st.columns(2)
 with col1:
-    suhuter = st.number_input("Masukkan Suhu(termometer)",min_value=0,max_value=50)
+    suhuter = st.number_input("Masukkan Suhu(termometer)")
 with col2:
-    suhusen = st.number_input("Masukkan Suhu(sensor)",min_value=0, max_value=50)
+    suhusen = st.number_input("Masukkan Suhu(sensor)")
 
-
-    #     bp = st.selectbox("Golongan Darah",("A","B","AB","O"))
 col3,col4 =st.columns(2)
 with col3:    
-   phmet = st.number_input("Masukkan pH(ph meter)",min_value=0, max_value=50)
+   phmet = st.number_input("Masukkan pH(ph meter)")
 with col4:
-      phsen = st.number_input("masukkan pH(ph sensor)", min_value=0 ,max_value=50)
+      phsen = st.number_input("masukkan pH(ph sensor)")
 columns = st.columns((2,3))
 
 
 submit = columns[1].button("Submit")
     # if sumbit and suhuter != 0 and suhusen != 0 and phmet != 0 and phsen != 0:
 if submit:
-    print([suhuter,suhusen,phmet,phsen])
+    temp =[suhuter,suhusen,phmet,phsen]
     data = dataset.modell([suhuter,suhusen,phmet,phsen])
-    data = dataset.cluster(data)
-    print(data)
+    data = dataset.svr(data)
+    data =  np.round(data)
 
-# suhumanual = data1['Temperature (°C)']
-# mean = data2["TEMP"].mean()
-# data2["TEMP"] = data2["TEMP"].replace(np.nan, mean)
-# suhuauto = data2['TEMP']
-# phmanual = data1['pH']
-# phauto = data2['pH']
-# # proses penggabungan data
-# data = pd.concat([suhumanual.rename("Suhu(termometer)"), suhuauto.rename('Suhu(sensor)') ,phmanual.rename('pH(ph meter)'),phauto.rename('pH(sensor)')] , axis=1)
-# X = data
+    if(data[-1] == 1):
+        st.write("Hasil Klasifikasi : (Berkualitas Atau Baik)")
+    else:
+        st.write("Hasil Klasifikasi : (Tidak Berkualitas Atau Buruk)")
+    
+    cur = mydb['data']
+    cur.insert_one({
+        "Suhu(termometer)": suhuter,
+        "Suhu(sensor)": suhusen,
+        "pH(ph meter)": phmet,
+        "pH(sensor)": phsen,
+        "Klasifikasi": data[-1]
+    })
 
-# if selected == "Dataset":
-#     _, col2, _ = st.columns([1, 1, 1])
-#     with col2:
-#         st.write('''## Dataset''')
-#         st.write('''\n''')
-#     st.write(X)
 
-# # """## Proses Preprosessing"""
+suhumanual = data1['Temperature (°C)']
+mean = data2["TEMP"].mean()
+data2["TEMP"] = data2["TEMP"].replace(np.nan, mean)
+suhuauto = data2['TEMP']
+phmanual = data1['pH']
+phauto = data2['pH']
+# proses penggabungan data
+data = pd.concat([suhumanual.rename("Suhu(termometer)"), suhuauto.rename('Suhu(sensor)') ,phmanual.rename('pH(ph meter)'),phauto.rename('pH(sensor)')] , axis=1)
+X = data
+# if selected == "Cek Kualitas":
+#     tab1
+# #     _, col2, _ = st.columns([1, 1, 1])
+# #     with col2:
+#         st.write('''Cek''')
+# #         st.write('''\n''')
+#     st.write()
+if selected == "Dataset":
+    _, col2, _ = st.columns([1, 1, 1])
+    with col2:
+        st.write('''## Dataset''')
+        st.write('''\n''')
+    st.write(X)
 
-# if selected == "Prepocessing":
-#     _, col2, _ = st.columns([1, 3, 1])
-#     with col2:
-#         st.write('''## Data setelah Prepocessing''')
-#         st.write('''\n''')
-#     scaler = MinMaxScaler()
-#     scaled = scaler.fit_transform(X)
-#     features_names = X.columns.copy()
-#     scaled_features = pd.DataFrame(scaled, columns=features_names)
-#     st.write(scaled_features.head(5))
+# """## Proses Preprosessing"""
 
-# # """## Proses Pengelompokan data
+if selected == "Prepocessing":
+    _, col2, _ = st.columns([1, 3, 1])
+    with col2:
+        st.write('''## Data setelah Prepocessing''')
+        st.write('''\n''')
+    scaler = MinMaxScaler()
+    scaled = scaler.fit_transform(X)
+    features_names = X.columns.copy()
+    scaled_features = pd.DataFrame(scaled, columns=features_names)
+    st.write(scaled_features.head(5))
 
-# # Pelabelan Menggunakana KMeans Clustering
-# # """
+# """## Proses Pengelompokan data
 
-# # menentukan 
-# scores = []
-# # Number of clusters
-# kmeans = KMeans(n_clusters=2)
-# # Fitting the input data
-# kmeans = kmeans.fit(X)
-# # Getting the cluster labels
-# labels = kmeans.predict(X)
-# y = labels
-# if selected == "Pelabelan":
-#     _, col2, _ = st.columns([1, 2, 1])
-#     with col2:
-#         st.header('Data yang dilabeli')
-#         st.write('''\n''')
+# Pelabelan Menggunakana KMeans Clustering
+# """
 
-#     label = pd.DataFrame(y)
-#     label.rename(columns = {0: "Label"}, inplace=True)
-#     new_data = pd.concat([data, label], axis=1)
-#     st.write(new_data)
+# menentukan 
+scores = []
+# Number of clusters
+kmeans = KMeans(n_clusters=2)
+# Fitting the input data
+kmeans = kmeans.fit(X)
+# Getting the cluster labels
+labels = kmeans.predict(X)
+y = labels
+if selected == "Pelabelan":
+    _, col2, _ = st.columns([1, 2, 1])
+    with col2:
+        st.header('Data yang dilabeli')
+        st.write('''\n''')
 
-# # """## Eksekusi Ke model SVM"""
+    label = pd.DataFrame(y)
+    label.rename(columns = {0: "Label"}, inplace=True)
+    new_data = pd.concat([data, label], axis=1)
+    st.write(new_data)
 
-# if selected == "Klasifikasi":
-#     _, col2, _ = st.columns([1, 2, 1])
-#     with col2:
-#         st.write("## Klasifikasi SVM")
-#     # proses Split data
-#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+# """## Eksekusi Ke model SVM"""
 
-#     # Fit model.
-#     model = SVR()
-#     model.fit(X_train, y_train)
+if selected == "Klasifikasi":
+    _, col2, _ = st.columns([1, 2, 1])
+    with col2:
+        st.write("## Klasifikasi SVM")
+    # proses Split data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-#     # Make a prediction and determine the error.
-#     y_test_prediction = model.predict(X_test)
-#     y_test_prediction = (np.round(y_test_prediction))
-#     test_rmse_svm = np.sqrt(accuracy_score(y_test, y_test_prediction))
+    # Fit model.
+    model = SVR()
+    model.fit(X_train, y_train)
 
-#     # Result
-#     st.write(f'svm-regressor-rmse  : {test_rmse_svm}')
+    # Make a prediction and determine the error.
+    y_test_prediction = model.predict(X_test)
+    y_test_prediction = (np.round(y_test_prediction))
+    test_rmse_svm = np.sqrt(accuracy_score(y_test, y_test_prediction))
 
+    # Result
+    st.write(f'svm-regressor-rmse  : {test_rmse_svm}')
